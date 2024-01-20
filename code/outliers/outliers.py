6051870ba4ee0affe19655d1710f64f168f46d5e
Nicolas Le Hir <nicolaslehir@gmail.com>
2024-01-20 13:32:16 +0100

    update outliers

--- code/outliers/outliers.py
+++ code/outliers/outliers.py
@@ -5,76 +5,45 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = np.load("data.npy")
 
-plt.plot(data[:, 0], data[:, 1], "o", markersize=1)
-plt.savefig("scatter plot.pdf")
-nb_points = data.shape[0]
+def compute_outliers(data, metric, factor):
+    center = np.mean(data, axis=0)
+    if metric == "euclidean":
+        metric = "euclidean"
+        differences = data - center
+        distance_to_center = np.linalg.norm(differences, axis=1)
+    elif metric == "custom":
+        std = data.std(axis=0)
+        differences = data - center
+        differences /= std
+        distance_to_center = np.linalg.norm(differences, ord=1, axis=1)
+    else:
+        raise ValueError("Unknown metric")
 
-"""
-    Compute center
-"""
-center = np.mean(data, axis=0)
+    mean_distance_to_center = distance_to_center.mean()
+    points_considered_outliers = data[distance_to_center > factor*mean_distance_to_center]
 
-"""
-    Compute mean distance to center with euclidean metric
-"""
-metric = "euclidean"
-differences = data - center
-distances_to_center = np.linalg.norm(differences, axis=1)
-mean_distance_to_center = distances_to_center.sum() / len(distances_to_center)
-
-nb_outliers = 0
-for index in range(nb_points):
-    datapoint = data[index]
-    vector_to_center = datapoint - center
-    distance_to_center = np.linalg.norm(vector_to_center)
-    if distance_to_center > 3 * mean_distance_to_center:
-        print(datapoint)
-        print(distance_to_center / mean_distance_to_center)
-        print("")
-        plt.plot(datapoint[0], datapoint[1], "o", color="red", markersize=1.5)
-        nb_outliers += 1
-print(f"{nb_outliers} outliers")
+    return points_considered_outliers
 
 
-plt.title(f"outliers in red, {metric} metric")
-plt.savefig(f"outliers, metric={metric}.pdf")
-plt.close()
+def plot_data_and_outliers(data, metric, factor):
+    plt.plot(data[:, 0], data[:, 1], "o", markersize=1)
+    outliers=compute_outliers(data=data, metric=metric, factor=factor)
+    for point in outliers:
+        plt.plot(point[0], point[1], "o", color="red", markersize=1.5)
+    plt.title(f"outliers in red, {metric} metric")
+    plt.savefig(f"outliers, metric={metric}.pdf")
+    plt.close()
 
+def main():
+    data = np.load("data.npy")
 
-plt.plot(data[:, 0], data[:, 1], "o", markersize=1)
-"""
-    Compute mean distance to center with a more adapted metric
-"""
-metric = "custom"
-std_x = np.std(data[:, 0])
-std_y = np.std(data[:, 1])
-# factor = 5
-differences = data - center
-differences[:, 0] /= std_x
-differences[:, 1] /= std_y
-abs_differences = np.abs(differences)
-distances = np.sum(abs_differences, axis=1)
-sum_distances = np.sum(distances)
-mean_distance_to_center = sum_distances / nb_points
+    # plot raw data
+    plt.savefig("scatter plot.pdf")
 
-nb_outliers = 0
-for index in range(nb_points):
-    datapoint = data[index]
-    vector_to_center = datapoint - center
-    distance_to_center = abs(vector_to_center[0] / std_x) + abs(
-        vector_to_center[1] / std_y
-    )
-    if distance_to_center > 2.5 * mean_distance_to_center:
-        print(datapoint)
-        print(distance_to_center / mean_distance_to_center)
-        print("")
-        plt.plot(datapoint[0], datapoint[1], "o", color="red", markersize=1.5)
-        nb_outliers += 1
-print(f"{nb_outliers} outliers")
+    # plot outliers with different parameters
+    plot_data_and_outliers(data=data, metric="euclidean", factor=3)
+    plot_data_and_outliers(data=data, metric="custom", factor=2.5)
 
-
-plt.title(f"outliers in red, {metric} metric")
-plt.savefig(f"outliers, metric={metric}.pdf")
-plt.close()
+if __name__ == "__main__":
+    main()
